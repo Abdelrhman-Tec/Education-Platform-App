ac5a1ce9c223a26ebd189297e60aa54db99b3708
Abdelrhman Nada <abdelrhman.nada.tech@gmail.com>
2025-11-08 04:24:02 +0200

    feat: Add index file to export home screen widgets

---
+++ lib/features/home/presentation/widgets/index.dart
@@ -0,0 +1,5 @@
+export  'package:education_platform_app/features/home/presentation/widgets/category_item.dart';
+export 'package:education_platform_app/features/home/presentation/widgets/section_title.dart';
+export '../widgets/home_app_bar.dart';
+export '../widgets/home_search_bar.dart';
+export '../widgets/title_header_page.dart';
